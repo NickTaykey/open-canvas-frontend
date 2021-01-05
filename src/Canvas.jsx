29ee995b59c6407fccf1603e5b5f422c8d851355
Niccolò Toccane <nico.toccane@gmail.com>
2021-01-05 18:36:04 +0100

    refact Canvas

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -4,70 +4,63 @@ import './Canvas.css';
 class Canvas extends Component {
 	constructor (props) {
 		super(props);
-		this.canvas = new React.createRef();
+		const { width, pixelsLen } = this.props;
+		this.pixelsOffset = width / pixelsLen;
+		this.canvasRef = new React.createRef();
 		this.state = { drawing: false };
-		this.pixelsOffset = this.props.width / this.props.pixelsLen;
+		this.canvas = null;
 		this.ctx = null;
 	}
 
 	componentDidMount () {
-		const ctx = this.canvas.current.getContext('2d');
-		ctx.fillStyle = 'red';
-		this.ctx = ctx;
+		this.canvas = this.canvasRef.current;
+		this.ctx = this.canvas.getContext('2d');
+		this.ctx.fillStyle = 'red';
 		this.drawGrid();
 	}
 
-	draw = e => {
-		const event = e.nativeEvent;
+	draw = ({ nativeEvent: e }) => {
 		if (this.state.drawing) {
-			if (event.type === 'mousemove' && event.which === 1) {
-				let xIndex = Math.floor(event.offsetX / this.pixelsOffset);
-				let yIndex = Math.floor(event.offsetY / this.pixelsOffset);
-				this.fillPixel(xIndex, yIndex);
+			const { canvas, pixelsOffset } = this;
+			let offsetX, offsetY;
+			if (e.type === 'mousemove' && e.which === 1) {
+				offsetX = e.offsetX;
+				offsetY = e.offsetY;
 			} else if (e.type === 'touchmove') {
-				let offsetX = event.touches[0].pageX - this.canvas.current.offsetLeft;
-				let offsetY = event.touches[0].pageY - this.canvas.current.offsetTop;
-				let xIndex = Math.floor(offsetX / this.pixelsOffset);
-				let yIndex = Math.floor(offsetY / this.pixelsOffset);
-				this.fillPixel(xIndex, yIndex);
+				offsetX = e.touches[0].pageX - canvas.offsetLeft;
+				offsetY = e.touches[0].pageY - canvas.offsetTop;
 			}
+			let xIndex = Math.floor(offsetX / pixelsOffset);
+			let yIndex = Math.floor(offsetY / pixelsOffset);
+			this.fillPixel(xIndex, yIndex);
 		}
 	};
 
 	fillPixel (xIndex, yIndex) {
-		this.ctx.fillRect(
-			xIndex * this.pixelsOffset,
-			yIndex * this.pixelsOffset,
-			this.pixelsOffset,
-			this.pixelsOffset
+		const { ctx, pixelsOffset } = this;
+		ctx.fillRect(
+			xIndex * pixelsOffset,
+			yIndex * pixelsOffset,
+			pixelsOffset,
+			pixelsOffset
 		);
-		/* pixels.push({
-			topLeftX : xIndex * this.props.pixelsLen,
-			topLeftY : Y_INDEX * this.props.pixxIndex * this.props.pixelsLen
-    }); */
 	}
 
 	drawGrid () {
 		const { width, height } = this.props;
-		this.ctx.beginPath();
-
-		// vertical lines
-
-		for (let x = this.pixelsOffset; x < width; x += this.pixelsOffset) {
-			this.ctx.moveTo(x, 0);
-			this.ctx.lineTo(x, width);
+		const { ctx, pixelsOffset } = this;
+		ctx.beginPath();
+		for (let x = pixelsOffset; x < width; x += pixelsOffset) {
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, width);
 		}
-
-		// horizontal lines
-
-		for (let y = this.pixelsOffset; y < height; y += this.pixelsOffset) {
-			this.ctx.moveTo(0, y);
-			this.ctx.lineTo(height, y);
+		for (let y = pixelsOffset; y < height; y += pixelsOffset) {
+			ctx.moveTo(0, y);
+			ctx.lineTo(height, y);
 		}
-
-		this.ctx.strokeStyle = 'light-grey';
-		this.ctx.lineWidth = 0.75;
-		this.ctx.stroke();
+		ctx.strokeStyle = 'light-grey';
+		ctx.lineWidth = 0.75;
+		ctx.stroke();
 	}
 
 	startDrawing = () => this.setState({ drawing: true });
@@ -76,7 +69,7 @@ class Canvas extends Component {
 	render () {
 		return (
 			<canvas
-				ref={this.canvas}
+				ref={this.canvasRef}
 				width={this.props.width}
 				height={this.props.height}
 				onMouseDown={this.startDrawing}
