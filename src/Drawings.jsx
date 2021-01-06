c56fc64237d9cb79a21f29865a54a3730694d9ac
Niccolò Toccane <nico.toccane@gmail.com>
2021-01-06 16:08:29 +0100

    implement react router

---
+++ src/Drawings.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Drawings extends Component {
+	render () {
+		return (
+			<div className="drawings">
+				<h1>All the drawings will be displayed here</h1>
+			</div>
+		);
+	}
+}
+
+export default Drawings;
--- src/OpenCanvas.jsx
+++ src/OpenCanvas.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import NewDrawing from './NewDrawing';
+import Drawings from './Drawings';
 
 class OpenCanvas extends Component {
 	render () {
 		return (
 			<div>
-				<NewDrawing />
+				<Switch>
+					<Route exact path="/drawings/new" component={NewDrawing} />
+					<Route exact path="/drawings" component={Drawings} />
+				</Switch>
 			</div>
 		);
 	}
